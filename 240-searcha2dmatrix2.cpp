c35f8ccf84b5761116c6517e81c0c50a582c0505
mahi3006 <mahisrihi@gmail.com>
2025-07-31 16:49:02 +0530

    practice

---
+++ 240-searcha2dmatrix2.cpp
@@ -0,0 +1,11 @@
+class Solution:
+    def searchMatrix(self, matrix, target):
+        i, j = 0, len(matrix[0]) - 1
+        while i < len(matrix) and j >= 0:
+            if matrix[i][j] == target:
+                return True
+            elif matrix[i][j] < target:
+                i += 1
+            else:
+                j -= 1
+        return False
