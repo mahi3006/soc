da6c0782bcd1b8969de4385492e2ffb9e98ed59a
mahi3006 <mahisrihi@gmail.com>
2025-07-31 16:44:03 +0530

    practice

---
+++ 134-gasstation.cpp
@@ -0,0 +1,11 @@
+class Solution:
+    def canCompleteCircuit(self, gas, cost):
+        if sum(gas) < sum(cost):
+            return -1
+        total = start = tank = 0
+        for i in range(len(gas)):
+            tank += gas[i] - cost[i]
+            if tank < 0:
+                tank = 0
+                start = i + 1
+        return start
